3600891b92185a62c55d86cacd9996ba373ab668
B1media <chenyishain@gmail.com>
2016-09-21 14:34:47 +0800

    add checkitem testing

--- Model/ItemManager.swift
+++ Model/ItemManager.swift
@@ -9,6 +9,20 @@
 import Foundation
 
 class ItemManager {
-    let toDoCount = 0
+    var toDoCount = 0
     let doneCount = 0
+    private var toDoItems = [ToDoItem]()
+    
+    
+    func addItem(item: ToDoItem) {
+        toDoCount += 1
+        //Swift 3.0 語法 （原本書中展示++toDoCount）
+        toDoItems.append(item)
+    }
+    func itemAtIndex(index: Int) -> ToDoItem {
+        return toDoItems[index]
+    }
+
+    func checkItemAtIndex(index: Int) {
+    }
 }
--- Model/ItemManagerTests.swift
+++ Model/ItemManagerTests.swift
@@ -34,5 +34,24 @@ class ItemManagerTests: XCTestCase {
                        "Initially done count should be 0")
     }
     
+    func testToDoCount_AfterAddingOneItem_IsOne() {
+        sut.addItem(item: ToDoItem(title: "Test title"))
+        XCTAssertEqual(sut.toDoCount, 1, "toDoCount should be 1")
+    }
+    
+    func testItemAtIndex_ShouldReturnPreviouslyAddedItem() {
+        let item = ToDoItem(title: "Item")
+        sut.addItem(item: item)
+        let returnedItem = sut.itemAtIndex(index: 0)
+        XCTAssertEqual(item.title, returnedItem.title,
+                       "should be the same item")
+    }
+
+    func testCheckingItem_ChangesCountOfToDoAndOfDoneItems() {
+        sut.addItem(item: ToDoItem(title: "First Item"))
+        sut.checkItemAtIndex(0)
+        XCTAssertEqual(sut.toDoCount, 0, "toDoCount should be 0")
+        XCTAssertEqual(sut.doneCount, 1, "doneCount should be 1")
+    }
    
 }
