da79edb9a2e499675c12ec7058bf1c5ddfccbd59
B1media <chenyishain@gmail.com>
2016-09-21 10:57:45 +0800

    add location test case

---
+++ Model/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  ToDo
+//
+//  Created by B1media on 2016/9/21.
+//  Copyright © 2016年 yishainChen. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+    let name: String
+}
--- Model/ToDoItemTests.swift
+++ Model/ToDoItemTests.swift
@@ -21,13 +21,39 @@ class ToDoItemTests: XCTestCase {
         super.tearDown()
     }
     
-    func testInit_ShouldTakeTitle() {
-        let item = ToDoItem(title: "Test title", itemDescription: nil)
+    func  testInit_ShouldSetTitle() {
+//        _ = ToDoItem(title: "Test title")
+        let item = ToDoItem(title: "Test title")
+        XCTAssertEqual(item.title, "Test title",
+                       "Initializer should set the item title")
     }
     
-    func testInit_ShouldTakeTitleAndDescription() {
-        _ = ToDoItem(title: "Test title",
-                     itemDescription: "Test description")
+    func testInit_ShouldSetTitleAndDescription() {
+        let item = ToDoItem(title: "Test title",itemDescription: "Test description")
+        XCTAssertEqual(item.itemDescription , "Test description",
+                       "Initializer should set the item description")
+    }
+    
+    func testInit_ShouldSetTitleAndDescriptionAndTimestamp() {
+        let item = ToDoItem(title: "Test title",
+                            itemDescription: "Test description",
+                            timestamp: 0.0)
+        XCTAssertEqual(0.0, item.timestamp,
+                       "Initializer should set the timestamp")
+    }
+    
+    func testInit_ShouldSetTitleAndDescriptionAndTimestampAndLocation() {
+        let location = Location(name: "Test name")
+    }
+    
+    func testInit_ShouldTakeTitleAndDescriptionAndTimestampAndLocation() {
+        let location = Location(name: "Test name")
+        let item = ToDoItem(title: "Test title",
+                            itemDescription: "Test description",
+                            timestamp: 0.0,
+                            location: location)
+        XCTAssertEqual(location.name, item.location?.name,
+                       "Initializer should set the location")
     }
     
 }
